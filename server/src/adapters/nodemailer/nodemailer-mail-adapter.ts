9ec2de89b738ac2ec83163ce21e8a9b539a87e5f
Filipe Ariel <filipe.arielp@gmail.com>
2022-06-14 21:07:29 -0300

    Widget Web

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "2fe114b987c8f9",
+    pass: "c7fa9a1f20037f"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Filipe Ariel <filipe.arielp@gmail.com>',
+      subject,
+      html: body
+    })
+  }
+}
