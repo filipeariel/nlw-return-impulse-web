8ffdb5582b2c0942d476569cfab2876412bb836f
Filipe Ariel <filipe.arielp@gmail.com>
2022-06-24 15:23:02 -0300

    mobile version added

---
+++ mobile/App.tsx
@@ -0,0 +1,34 @@
+import 'react-native-gesture-handler'
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+import Widget from './src/components/Widget';
+import { theme } from './src/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <></>;
+  }
+
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+      <StatusBar
+        style="light"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Widget />
+    </View>
+  );
+}
